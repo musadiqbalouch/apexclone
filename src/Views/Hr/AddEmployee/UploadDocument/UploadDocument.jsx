e64f64fb4c1ab80d463e76f100bf40c86dc8d31d
musadiqbalouch <musadiqbalouchi321@gmail.com>
2026-01-07 20:04:22 +0500

    created-uploaddocument-component

--- src/Views/Hr/AddEmployee/UploadDocument/UploadDocument.jsx
+++ src/Views/Hr/AddEmployee/UploadDocument/UploadDocument.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { TbPhoto } from "react-icons/tb";
+import CommonButton from "../../../../Components/Common/CommonButton/CommonButton";
+// import { UploadDocument } from "../../../../Components/Constants/HrConstantsData/HrConstantData";
+import { Documents } from "../../../../Components/Constants/HrConstantsData/HrConstantData";
 
 const UploadDocument = () => {
-  return <div>UploadDocument</div>;
+  return (
+    <div className="grid grid-cols-2 items-center gap-5 mt-2">
+      {Documents.map((document) => (
+        <div className="w-full border-gray-200 flex items-center border h-20 mt-2 rounded-md px-3 gap-3">
+          <div className="bg-gray-300 w-fit  text-center p-5 rounded-3xl">
+            <TbPhoto className=" text-gray-600 h-7 w-7 text-center" />
+          </div>
+          <div className="w-full">
+            <label className="text-base font-semibold">{document.title}</label>
+            <p className="text-gray-800 text-sm font-semibold">
+              {document.imageSize}
+            </p>
+            <CommonButton className={""} text={"Upload"} />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default UploadDocument;
